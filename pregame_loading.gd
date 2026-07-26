9e1b15066b27fc565c4d55a652f39a617da8c120
qujimin <rafsil36@kingshigh.school.nz>
2026-07-27 11:01:49 +1200

    TUNGUTNUGNUTNUNTNGNGUNTUTNTTTT

--- pregame_loading.gd
+++ pregame_loading.gd
@@ -6,28 +6,37 @@ signal loading_screen_ready
 @export var progress_bar : ProgressBar
 @export var percentage_label : Label
 @export var spinner : TextureRect
+@export var min_display_time : float = 3.0
 
 var update_value : float = 0.0
+var is_loading : bool = false
 
 func _ready() -> void:
+	spinner.pivot_offset = spinner.size / 2.0
 	progress_bar.max_value = 1.0
 	progress_bar.value = 0.0
 	await animation_player.animation_finished
+	await get_tree().create_timer(min_display_time).timeout
+	is_loading = true
 	loading_screen_ready.emit()
-
-func _process(delta: float) -> void:
-	spinner.rotation_degrees += 180.0 * delta
-	var target : float = 0.5 if update_value >= 1.0 else clamp(update_value + (0.9 - progress_bar.value) * delta * 0.5, 0.0, 0.9)
-	progress_bar.value = lerp(progress_bar.value, target, delta * 3.0)
-	percentage_label.text = str(int(progress_bar.value * 100))
 
 func on_progress_changed(value: float) -> void:
 	if value > update_value:
 		update_value = value
 
+func _process(delta: float) -> void:
+	spinner.rotation_degrees = wrapf(spinner.rotation_degrees + 180.0 * delta, 0.0, 360.0)
+	if not is_loading:
+		return
+	if progress_bar.value < update_value:
+		progress_bar.value = lerp(progress_bar.value, update_value, delta)
+	progress_bar.value += delta * 0.2 * \
+		(0.5 if update_value >= 1.0 else clamp(0.9 - progress_bar.value, 0.0, 1.0))
+	percentage_label.text = str(int(progress_bar.value * 100.0)) + " %"
+
 func on_load_finished() -> void:
 	progress_bar.value = 1.0
-	percentage_label.text = "100"
+	percentage_label.text = "100 %"
 	animation_player.play_backwards("Transition")
 	await animation_player.animation_finished
 	queue_free()
--- scene_loader.gd
+++ scene_loader.gd
@@ -1,11 +1,59 @@
 extends Node
 
+signal progress_changed(value: float)
+signal load_finished
 
-# Called when the node enters the scene tree for the first time.
+const LOADING_SHORT     : PackedScene = preload("res://loading_screen_short.tscn")
+const LOADING_LONG      : PackedScene = preload("res://loading_screen_long.tscn")
+const PREGAME_LOADING   : PackedScene = preload("res://pregame_loading.tscn")
+
+var scene_path : String = ""
+var previous_scene : String = ""
+var progress : Array = []
+var use_sub_threads : bool = true
+
 func _ready() -> void:
-	pass # Replace with function body.
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	set_process(false)
 
+func load_scene(_scene_path: String, transition_type: String = "short") -> void:
+	previous_scene = get_tree().current_scene.scene_file_path
+	scene_path = _scene_path
+	var loading_screen : PackedScene
+	match transition_type:
+		"short":
+			loading_screen = LOADING_SHORT
+		"long":
+			loading_screen = LOADING_LONG
+		"pregame":
+			loading_screen = PREGAME_LOADING
+		_:
+			loading_screen = LOADING_SHORT
+	var new_load_screen := loading_screen.instantiate()
+	add_child(new_load_screen)
+	new_load_screen.process_mode = Node.PROCESS_MODE_ALWAYS
+	progress_changed.connect(new_load_screen.on_progress_changed)
+	load_finished.connect(new_load_screen.on_load_finished)
+	await new_load_screen.loading_screen_ready
+	_start_load()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func go_to_previous() -> void:
+	if previous_scene != "":
+		load_scene(previous_scene)
+
+func _start_load() -> void:
+	var state : Error = ResourceLoader.load_threaded_request(scene_path, "", use_sub_threads)
+	if state == OK:
+		set_process(true)
+
+func _process(_delta: float) -> void:
+	var status : int = ResourceLoader.load_threaded_get_status(scene_path, progress)
+	progress_changed.emit(progress[0])
+	match status:
+		ResourceLoader.THREAD_LOAD_FAILED, ResourceLoader.THREAD_LOAD_INVALID_RESOURCE:
+			set_process(false)
+		ResourceLoader.THREAD_LOAD_LOADED:
+			set_process(false)
+			var resource = ResourceLoader.load_threaded_get(scene_path)
+			get_tree().change_scene_to_packed(resource)
+			load_finished.emit()
